714644805c4c8bf9689107305256f6bbca61cd0f
Ishita Gupta <66175970+ishitaaagupta@users.noreply.github.com>
2024-11-20 18:25:18 +0530

    minor changes

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://munchify-backend.vercel.app/reservation/send",
+        "https://vercel.com/ishitaaaguptas-projects/munchify-backend/GgE7PoNdY4DiJqoqea1Yi1oQTUXy/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
