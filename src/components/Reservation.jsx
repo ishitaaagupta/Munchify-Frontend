9c666c21e62c4b17ba30f964e7a3257f26d45ee4
Ishita Gupta <66175970+ishitaaagupta@users.noreply.github.com>
2024-11-20 18:00:54 +0530

    added deployed url

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3000/api/v1/reservation/send",
+        "https://munchify-backend.vercel.app/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
