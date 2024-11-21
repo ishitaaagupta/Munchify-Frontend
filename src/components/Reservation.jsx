8f0ad63733cce4d47d384f6d9885bd03e9b749b0
Ishita Gupta <66175970+ishitaaagupta@users.noreply.github.com>
2024-11-21 11:04:02 +0530

    minor changes

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -26,7 +26,7 @@ const Reservation = () => {
           headers: {
             "Content-Type": "application/json",
           },
-          withCredentials: true,
+          // withCredentials: true,
         }
       );
       toast.success(data.message);
